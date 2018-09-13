7e1bf08df95db5f83f90debb2a464eb2187dc2f8
DJ73 <d.max1998@gmail.com>
2018-09-13 18:56:37 +0530

    Added basic project login

---
+++ login.py
@@ -0,0 +1,18 @@
+from Tkinter import *
+
+# Login window
+root = Tk()
+root.title(string = 'ICE')
+
+Label(root, text = 'Project', font = 'Consolas 24', relief = FLAT).grid(row = 0, column = 0, sticky = 'nwe')
+
+entry = Entry(root, width = 25, font = 'Garamond 12', justify = CENTER)
+entry.grid(row = 1, column = 0, ipady=5)
+
+btn1 = Button(root, text = 'New Project', font = 'Monospace 16', relief = RIDGE)
+btn2 = Button(root, text = 'Join Project', font = 'Monospace 16', relief = RIDGE)
+
+btn1.grid(row = 2, column = 0, sticky = 'ew')
+btn2.grid(row = 3, column = 0, sticky = 'ew')
+
+root.mainloop()
