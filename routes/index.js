2c934f9f07a3f168a6b95006b9ec33e607c15110
Kashish Sharma <kashish25798@gmail.com>
2018-09-23 23:15:18 +0530

    Add: User now landing to editor activity

--- routes/index.js
+++ routes/index.js
@@ -8,76 +8,113 @@ var firebaseAdmin = admin.initializeApp({
   databaseURL: "https://ice18-fe14e.firebaseio.com"
 });
 
+
 var db = admin.firestore();
 const settings = {timestampsInSnapshots: true};
 db.settings(settings);
 
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 16; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ICE-Home', isUser: false });
 });
+
+router.get('/:uname/:status', function(req, res, next) {
+	var status = req.params.status
+	var uname = req.params.uname
+  res.render('index', { title: 'ICE-Home', isUser: status, uname: uname });
+});
+
+router.get('/editor/:id/:projectName', function(req, res, next){
+	var projectName = req.params.projectName;
+	var projectId = req.params.id;
+	db.collection("projects").where('id', "==", projectId)
+    .get()
+    .then(function(querySnapshot) {
+		if(querySnapshot.size != 0){
+			res.render('editor',{projectId: projectName});
+		} else{
+			res.send('Unauthorised access !!');
+		}
+	})
+	.catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+
+	
+});
+
 
 /* POST create new project from home page */
-router.post('/createNewProject', function(req, res, next){
-	var pname = req.body.pname;
+router.post('/submitHomeUserName', function(req, res, next){
 	var uname = req.body.uname;
+	var uid = makeid();
 
-	var projectId;
-	var userId;
-	
-	var addDoc = db.collection('projects').add({
-		name: pname[0],
-		timestamp: new Date().getTime()
-	}).then(projectRef => {
-			projectId = projectRef.id;
-			console.log('Added project with ID: ', projectRef.id);
-			db.collection('users').add({
-				name: pname[1],
+	db.collection("users").where("name", "==", uname)
+    .get()
+    .then(function(querySnapshot) {
+        if(querySnapshot.size != 0){
+			querySnapshot.forEach(function(doc) {
+				console.log(doc.id, " => ", doc.data());
+				res.redirect('/'+uname+'/'+true)
+			});
+		} else {
+			db.collection("users").doc(uname).set({
+				id: uid,
+				name: uname,
 				timestamp: new Date().getTime()
 			})
-			.then(userRef => {
-				userId = userRef.id;
-				console.log('Added user with ID: ', userRef.id);
-				db.collection('projects').doc(projectId)
-					.collection('users').doc(userId).set({
-						name: pname[1],
-						id: userId,
-						timestamp: new Date().getTime()
-					})
-					.then(subProjectRef => {
-						console.log('Added user in project with ID: ', subProjectRef.id);
-						db.collection('users').doc(userId)
-							.collection('projects').doc(projectId).set({
-								name: pname[0],
-								id: projectId,
-								timestamp: new Date().getTime()
-							})
-							.then(subUserRef => {
-								console.log("Everything completed");
-								res.redirect('/'+projectRef.id+'/editor');
-							})
-							.catch(error => {
-								console.log('Error adding project in user: ', error);
-							})
-					})
-					.catch(error => {
-						console.log('Error adding user in project: ', error);
-					})
+			.then(userRef =>{
+				res.redirect('/'+uname+'/'+true)
 			})
-			.catch(error => {
-				console.log('Error adding user: ', error);
-			})
-	})
-	.catch(error => {
-		console.log('Error adding project: ', error);
-	})
-
+			.catch(function(error) {
+				console.log("Error creating documents: ", error);
+			});
+		}
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+	
 });
 
+router.post('/submitHomeProjectName', function(req, res, next){
+	var pname = req.body.pname;
+	var pid = makeid();
 
-router.get('/:projectId/editor', function(req, res, next){
-	var projectId = req.params.projectId;
-	res.render('editor',{projectId: projectId});
+	db.collection("projects").where("name", "==", pname)
+    .get()
+    .then(function(querySnapshot) {
+        if(querySnapshot.size != 0){
+			querySnapshot.forEach(function(doc) {
+				console.log(doc.id, " => ", doc.data());
+				res.redirect('/editor/'+doc.data().id+'/'+pname)
+			});
+		} else {
+			db.collection("projects").doc(pname).set({
+				id: pid,
+				name: pname,
+				timestamp: new Date().getTime()
+			})
+			.then(projectRef =>{
+				res.redirect('/editor/'+pid+'/'+pname)
+			})
+			.catch(function(error) {
+				console.log("Error creating project: ", error);
+			});
+		}
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
 });
 
 module.exports = router;
